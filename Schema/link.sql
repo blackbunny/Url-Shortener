1453d2d6cfbeb8d37e24ec71a448521d99ecf9d6
Murat Kucukosman <blackbunny@gmail.com>
2013-08-02 04:17:05 +0300

    schema

---
+++ Schema/link.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 02, 2013 at 04:16 AM
+-- Server version: 5.5.32-0ubuntu0.13.04.1
+-- PHP Version: 5.4.9-4ubuntu2.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `link`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `counts`
+--
+
+CREATE TABLE IF NOT EXISTS `counts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `links_id` int(11) unsigned NOT NULL,
+  `value` int(11) unsigned NOT NULL,
+  `visit_date` datetime NOT NULL,
+  `visitor_ip` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `links_id` (`links_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `longurl` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `visitor_count` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `counts`
+--
+ALTER TABLE `counts`
+  ADD CONSTRAINT `counts_ibfk_1` FOREIGN KEY (`links_id`) REFERENCES `links` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
