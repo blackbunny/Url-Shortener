72666ed43a5a4cf8d9d0a029af6753df9c839bf8
Murat Kucukosman <blackbunny@gmail.com>
2013-08-02 04:31:20 +0300

    new config

---
+++ app/config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+return new \Phalcon\Config(array(
+	'database' => array(
+		'adapter'  => 'Mysql',
+		'host'     => 'localhost',
+		'username' => 'root',
+		'password' => 'pass',
+		'name'     => 'link',
+	),
+	'application' => array(
+		'controllersDir' => __DIR__ . '/../../app/controllers/',
+		'modelsDir'      => __DIR__ . '/../../app/models/',
+		'viewsDir'       => __DIR__ . '/../../app/views/',
+		'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+		'libraryDir'     => __DIR__ . '/../../app/library/',
+		'baseUri'        => '/',
+                'domain'        => 'link',
+	),
+	'models' => array(
+		'metadata' => array(
+			'adapter' => 'Memory'
+		)
+	)
+));
